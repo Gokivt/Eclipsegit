5882e4a5c2ea0b2b844190fb6ae4f0e50f946862
Gokivt <gokivt@gmail.com>
2023-05-09 22:47:47 +0530

    This is my first git commit from eclipse

---
+++ git_training/src/git_training_package/Git_class.java
@@ -0,0 +1,10 @@
+package git_training_package;
+
+public class Git_class {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first training in eclipse for git");
+
+	}
+
+}
