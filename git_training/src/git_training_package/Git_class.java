d8191c47eb74fa424aec237bcddd4ef10f4d2d05
Gokivt <131644971+Gokivt@users.noreply.github.com>
2023-05-10 07:56:21 +0530

    Update made in git hub Git_class.java

--- git_training/src/git_training_package/Git_class.java
+++ git_training/src/git_training_package/Git_class.java
@@ -6,6 +6,7 @@ public class Git_class {
 		System.out.println("This is my first training in eclipse for git");
 		System.out.println("This is my first coding in eclipse");
 		System.out.println("Changes pushd via Git");
+		System.out.println("Changes made in git hub");
 
 	}
 
