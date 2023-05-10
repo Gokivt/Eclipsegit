3befec84852c6ff240608c8290db798f6b2938ff
Gokivt <gokivt@gmail.com>
2023-05-10 07:50:14 +0530

    Pushing the changes via git cmd

--- git_training/src/git_training_package/Git_class.java
+++ git_training/src/git_training_package/Git_class.java
@@ -5,6 +5,7 @@ public class Git_class {
 	public static void main(String[] args) {
 		System.out.println("This is my first training in eclipse for git");
 		System.out.println("This is my first coding in eclipse");
+		System.out.println("Changes pushd via Git");
 
 	}
 
