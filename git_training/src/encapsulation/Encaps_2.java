7c5101d7fc7e131d0ec0ffa0eda2a9eedb3df42f
Gokivt <gokivt@gmail.com>
2023-08-15 23:41:31 +0530

    New codes were added

--- git_training/src/encapsulation/Encaps_2.java
+++ git_training/src/encapsulation/Encaps_2.java
@@ -9,6 +9,7 @@ public class Encaps_2 extends Encaps_1 {
 		obj.setAccNo(12345);
 		obj.setEmail("Eamil@email.com");
 		
+		
 		System.out.println("Account Number is: "+obj.getAccNo());
 		System.out.println("Account Name is: "+obj.getAccName());
 		System.out.println("Email address is: "+obj.getEamiladdress());
--- git_training/src/inheritancepackage/HDFC.java
+++ git_training/src/inheritancepackage/HDFC.java
@@ -5,11 +5,13 @@ public class HDFC extends RBI {
 	int iHDFC=2123;
 	String sHDFCBranch="Adayar";
 	
-	public HDFC() {
+	
+	public HDFC(String sName) {
 		//super(); --> By default this method will be invoked until the created constructor is not having parameter when it finds the keyword extends. 
 		//This is used to call the constructor from the immediate parent class
 		super(100); //--> Super method needs to call explicitly when the immediate parent class created constructor created with parameter
 		System.out.println("This is default HDFC constructor!!!");
+		System.out.println(sName);
 	}
 
 	public void display() {
@@ -23,7 +25,8 @@ public class HDFC extends RBI {
 	}
 	public static void main(String[] args) {
 		
-		HDFC oHDFC=new HDFC();
+		HDFC oHDFC=new HDFC("HDFC");
+		
 		oHDFC.display();
 		
 
